f8cebcdc7e2192d586b0026085de1b0e4975461b
Stuart McMahon <stuartlmcmahon@gmail.com>
2009-12-24 14:28:27 +1100

    Rewrite of task.py to keep Lewis happy. - it no long uses a lock, only a semaphore - it now contains a run() function which the user should call to run the task Note that it is untested. Usage would look like this:
    def printHello()
      print "Hello"
    
    def printWorld()
      print " world"
    
    helloTask = task(printHello)
    helloTask.run()
    worldTask = task(printWorld)
    worldTask.dependsOn(helloTask)
    worldTask.run()

--- src/cake/task.py
+++ src/cake/task.py
@@ -1,45 +1,52 @@
 import threading
 
 class Task:
-  """A class that calls a list of functions.
+  """A class that wraps callable functions to allow dependencies
+  between them.
   """  
-  def __init__(self, name):
-    """Constructor
+  def __init__(self, function):
+    """Construct a task to wrap a callable function.
+    
+    @param function: The callable function to run when run() is called.
     """    
-    self.__name = name
     self.__semaphore = threading.Semaphore(0)
-    self.__lock = threading.Lock()
-    self.__functions = []
+    self.__dependents = [ function ]
   
   def __call__(self):
-    """Call this task. The task will complete if all dependencies
-       have been completed.
+    """Call this task.
+    
+    The task will run if all dependencies have completed.
     """
-    self.complete()
+    self.run()
   
-  def complete(self):
-    """Complete this task. The task will complete only if all dependencies
-       have been completed
+  def run(self):
+    """Run this task.
+    
+    Note that the task may not actually run until all dependencies have
+    completed. 
     """
     if not self.__semaphore.acquire(False):
-      with self.__lock:
-        for f in self.__functions:
-          f()
-        self.__functions = None # Signal task done
+      try:
+        function = self.__dependents.pop(0)
+      except KeyError:
+        return # No more functions to run 
+      function()
   
-  def dependsOn(self, other):
-    """Makes sure this task completes after the 'other' task is completed.
+  def dependsOn(self, otherTask):
+    """Add a dependency to this task.
+    
+    This task will not run until 'otherTask' task has completed.
+    
+    @param otherTask: The task to be dependent on.
     """
     self.__semaphore.release()    
-    other.runWhenComplete(self.complete)
+    otherTask.addDependent(self.run)
     
-  def runWhenComplete(self, function):
-    """Registers a function to be run when the task is completed.
+  def addDependent(self, function):
+    """Registers a callable function to be run when this task has completed.
+    
+    @param function: A callable function to run when this task has completed.
     """    
-    with self.__lock:
-      if self.__functions is None:
-        # Already done this task, run the function now
-        function()
-      else:
-        # Wait for task to complete 
-        self.__functions.append(function)  
+    self.__dependents.append(function)
+    self.__semaphore.release()    
+    self.run()  

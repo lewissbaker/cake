76b2fdd0455ad1a11875bda1de38f8fadae12ead
Stuart McMahon <stuartlmcmahon@gmail.com>
2010-01-18 19:21:27 +1100

    cleared out all unecessary functionality from threadpool. it's now very light-weight (100 lines). max queue size functionality can be added again using semaphores if req'd

--- src/cake/test/threadpool.py
+++ src/cake/test/threadpool.py
@@ -1,29 +1,37 @@
 import unittest
+import threading
+
 from cake.threadpool import ThreadPool
 
 class ThreadPoolTests(unittest.TestCase):
 
   def testSingleJob(self):
     result = []
+    e = threading.Event()
     def job():
       result.append(None)
+      e.set()
        
     threadPool = ThreadPool(numWorkers=10)
     threadPool.queueJob(job)
-    threadPool.waitForJobsToComplete()
+    e.wait()
     threadPool.shutdown()
     
     self.assertEqual(len(result), 1)
     
   def testMultipleJobs(self):
+    jobCount = 50
     result = []
+    s = threading.Semaphore(0)
     def job():
       result.append(None)
+      s.release()
        
     threadPool = ThreadPool(numWorkers=10)
-    for _ in xrange(50):
+    for _ in xrange(jobCount):
       threadPool.queueJob(job)
-    threadPool.waitForJobsToComplete()
+    for _ in xrange(jobCount):
+      s.acquire()
     threadPool.shutdown()
     
     self.assertEqual(len(result), 50)
--- src/cake/threadpool.py
+++ src/cake/threadpool.py
@@ -4,7 +4,6 @@ in parallel on separate threads.
 
 import threading
 import win32api
-import time
 import sys
 import traceback
 
@@ -23,41 +22,21 @@ class JobQueue(object):
     """    
     self._jobSemaphore = threading.Semaphore(0)
     self._jobs = []
-    self._submitted = 0
-    self._completed = 0
-    self._completedLock = threading.Lock()
       
   def get(self):
     """Get the next job from the back of the queue. Blocks until a job is
     available.
     """
-    self._jobSemaphore.acquire()
-    job = self._jobs.pop()
-    return lambda job=job: self._executeJob(job)
+    self._jobSemaphore.acquire() # Wait for next job
+    return self._jobs.pop()
     
   def put(self, job, index=0):
     """Put a job on the queue at a given index. Defaults to putting the job
     on the front of the queue.
     """
     self._jobs.insert(index, job)
-    self._submitted += 1 # Must increase count before signalling a new job 
     self._jobSemaphore.release() # Signal a new job
- 
-  @property
-  def outstandingJobCount(self):
-    """Returns the number of jobs outstanding.
-    """    
-    return self._submitted - self._completed
-
-  def _executeJob(self, job):
-    """Execute a job and signal when it's complete.
-    """
-    try:
-      job()
-    finally:
-      with self._completedLock:
-        self._completed += 1
-          
+           
 class ThreadPool(object):
   """Manages a pool of worker threads that it delegates jobs to.
   
@@ -65,30 +44,20 @@ class ThreadPool(object):
   | pool = ThreadPool(numWorkers=4)
   | for i in xrange(50):
   |   pool.queueJob(lambda i=i: someFunction(i))
-  | pool.waitForJobsToComplete()
   """
-  class ExitThreadException(Exception):
-    """An exception used to signal thread exit.
-    """
-    pass
+  EXIT_JOB = "exit job"
   
-  def __init__(self, numWorkers, maxQueueSize=0):
+  def __init__(self, numWorkers):
     """Initialise the thread pool.
     
     @param numWorkers: Initial number of worker threads to start.
-    @param maxQueueSize: Maximum size of the job queue before adding new jobs
-    blocks the caller.
     """
-    self._maxQueueSize = maxQueueSize
     self._jobQueue = JobQueue()
     self._workers = []
 
     # Create the worker threads
     for _ in xrange(numWorkers):
       worker = threading.Thread(target=self._runThread)
-# HACK: Make the thread a daemon otherwise the KeyboardInterrupt can
-#  cause issues with thread shutdown. Does this problem still exist?
-      #worker.setDaemon(True)
       worker.start()
       self._workers.append(worker)
     
@@ -102,49 +71,34 @@ class ThreadPool(object):
     """On shutdown we complete any currently executing jobs then exit. Jobs
     waiting on the queue may not be executed.
     """
-    # Submit the exit job to the back of the queue
+    # Submit the exit job directly to the back of the queue
     for _ in xrange(len(self._workers)):
-      self._jobQueue.put(self._exitJob, -1)
+      self._jobQueue.put(self.EXIT_JOB, -1)
 
     # Wait for the threads to finish      
     for thread in self._workers:
       thread.join()
 
+    # Clear any references
     self._jobQueue = JobQueue()
     self._workers[:] = []
     
   def queueJob(self, callable):
-    """Queue a new job to be executed by the thread pool. Blocks if
-    there are already self.maxSize jobs in the queue. 
+    """Queue a new job to be executed by the thread pool.
     """
-    if self._maxQueueSize > 0:
-      self.waitForJobsToComplete(self._maxQueueSize - 1)
-      
     self._jobQueue.put(callable)
-  
-  def waitForJobsToComplete(self, outstandingJobCount=0):
-    """Wait for all but outstandingJobCount jobs to be completed.
-    
-    @param outstandingJobCount: The number of outstanding jobs
-    allowed to be left in the queue before returning.
-    """
-    while self._jobQueue.outstandingJobCount > outstandingJobCount:
-      time.sleep(0.1)
   
   def _runThread(self):
     """Process jobs continuously until dismissed.
     """
     while True:
       job = self._jobQueue.get()
+      if job is self.EXIT_JOB:
+        break
+
       try:
         job()
-      except self.ExitThreadException:
-        break # Exit the thread
       except Exception:
-        sys.stderr.write("Uncaught exception\n")
+        sys.stderr.write("Uncaught Exception:\n")
         sys.stderr.write(traceback.format_exc())
-
-  def _exitJob(self):
-    """Thread exit job.
-    """
-    raise self.ExitThreadException()
+        

0d0347ab920fc09720eb9e53f29f95a5108f3270
Stuart McMahon <stuartlmcmahon@gmail.com>
2010-02-18 15:58:59 +1100

    - moved main entrypoint to cakemain/__init__.py - renamed cake/main.py to cake/runner.py

--- src/cakemain/__init__.py
+++ src/cakemain/__init__.py
@@ -1,10 +1,22 @@
-"""Minimal bootstrapping module.
+"""Minimal main entrypoint.
+
+This main module reduces the risk of a stack dump caused by
+a KeyboardIntterupt by not loading any modules until a
+try-except block is in place.  
 """
 
 def run():
+  """Minimal run function.
+  """
   try:
+    import sys
     import cake.main
-    return cake.main.run()
+    sys.exit(cake.main.run())
   except KeyboardInterrupt:
     import sys
     sys.exit(-1)
+
+if __name__ == '__main__':
+  """Minimal main function.
+  """
+  run()

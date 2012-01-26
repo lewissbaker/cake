a0408a5fa12abb653979cb3739cba50725fbf4f6
Stuart McMahon <stuartlmcmahon@gmail.com>
2012-01-26 22:59:21 +1100

    - Made registry.py Python 3.x friendly.

--- src/cake/registry.py
+++ src/cake/registry.py
@@ -4,7 +4,7 @@
 @copyright: Copyright (c) 2010 Lewis Baker, Stuart McMahon.
 @license: Licensed under the MIT license.
 """
-import _winreg
+import _winreg as winreg # Do this so Python 2to3 conversion works.
 import sys
 
 import cake.system
@@ -18,7 +18,7 @@ def queryString(key, subKey, name):
   the 64-bit registry. If the value doesn't exist there it will then try to
   find the value in the 32-bit registry.
   
-  @param key: The key to query, eg: _winreg.HKEY_LOCAL_MACHINE
+  @param key: The key to query, eg: winreg.HKEY_LOCAL_MACHINE
   @type key: string
   
   @param subKey: The subkey to query, eg: r"SOFTWARE\Microsoft"
@@ -33,32 +33,32 @@ def queryString(key, subKey, name):
   @raise WindowsError: If the value could not be found. 
   """
   # List of access modes to try.
-  sams = [_winreg.KEY_READ]
+  sams = [winreg.KEY_READ]
   
   # Also try for a 32-bit registry key on 64-bit Windows. On 64-bit Windows
   # the Windows SDK is usually installed in the 64-bit program files
   # directory but the compiler is usually installed in the 32-bit program
   # files directory.
   if cake.system.isWindows64():
-    if hasattr(_winreg, "KEY_WOW64_32KEY"):
-      sams.append(_winreg.KEY_READ | _winreg.KEY_WOW64_32KEY)
+    if hasattr(winreg, "KEY_WOW64_32KEY"):
+      sams.append(winreg.KEY_READ | winreg.KEY_WOW64_32KEY)
     else:
       global _shownWow64Warning
       if not _shownWow64Warning:
         _shownWow64Warning = True
         sys.stderr.write(
-          "warning: _winreg module does not have access key KEY_WOW64_32KEY. "
+          "warning: winreg module does not have access key KEY_WOW64_32KEY. "
           "It may not be possible to find all compiler and SDK install "
           "locations automatically.\n"
           )
 
   for sam in sams:
     try:
-      keyHandle = _winreg.OpenKey(key, subKey, 0, sam)
+      keyHandle = winreg.OpenKey(key, subKey, 0, sam)
       try:
-        return str(_winreg.QueryValueEx(keyHandle, name)[0])
+        return str(winreg.QueryValueEx(keyHandle, name)[0])
       finally:
-        _winreg.CloseKey(keyHandle)
+        winreg.CloseKey(keyHandle)
     except WindowsError:
       if sam is sams[-1]:
         raise
